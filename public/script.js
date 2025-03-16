fc0e133f6f9f017b36142ff5639e0b5f01c284da
tsugumii21 <tsugumii21@gmail.com>
2025-03-16 22:43:53 +0800

    Updated index.html, about.html, services.html, contact.html, style.css, and script.js

--- public/script.js
+++ public/script.js
@@ -7,3 +7,17 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
     });
   });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleButton = document.getElementById("toggleButton");
+  const offcanvasNavbar = document.getElementById("offcanvasNavbar");
+
+  toggleButton.addEventListener("click", function () {
+    offcanvasNavbar.classList.toggle("show");
+  });
+});
+
+function toggleHomeButton() {
+  const toggleButton = document.getElementById("toggleButton");
+  toggleButton.innerHTML = '<a href="index.html" class="text-white">Home</a>';
+}
